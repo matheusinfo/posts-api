264f14dd7e65e03e39e5ae5c4b0e8edfbcb62e83
Matheus Andrade <matheus.add12@gmail.com>
2021-05-21 15:30:41 -0300

    feat: add DeletePost route

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,6 +1,15 @@
 <a href={{ route('posts.create') }}>Criar novo post</a>
+
+@if(session('message'))
+    <div>
+        {{ session('message') }}
+    </div>
+@endif
+
 <h1>Posts</h1>
 
 @foreach($posts as $post)
-    <p>{{ $post->title }}</p>
+    <p>
+        {{ $post->title }} [<a href="{{route('posts.show', $post->id)}}">Ver detalhes</a>]
+    </p>
 @endforeach
